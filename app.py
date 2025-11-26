19192f189e5f5abfb21e6944fef327b157737c74
urayamastudentdev-web <urayama.student.dev@gmail.com>
2025-11-26 10:22:25 +0900

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ generation_config = {
 }
 
 model = genai.GenerativeModel(
-    model_name='models/gemini-1.5-flash',
+    model_name='models/gemini-2.5-flash',
     generation_config=generation_config
 )
 
